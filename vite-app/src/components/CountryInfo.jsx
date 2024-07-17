eccc5cd54f663b077f7eabcd14f63e7597d736df
onnimiettinen <onnimie@gmail.com>
2024-07-17 19:02:15 +0300

    fix bug in CountryInfo component

--- vite-app/src/components/CountryInfo.jsx
+++ vite-app/src/components/CountryInfo.jsx
@@ -1,23 +1,28 @@
 const CountryInfo = ({countryObj}) => {
-    return (
-        <div>
-            <h2>
-                {countryObj.name}
-            </h2>
-            
-            <p>capital {countryObj.capital[0]}</p>
-            <p>area {countryObj.area} </p>
-            <br />
-            <b>languages:</b>
-            <ul>
-                {countryObj.languages.values().map(lang =>
-                    <li key={lang}>
-                        {lang}
-                    </li>
-                )}
-            </ul>
-        </div>
-    )
+    if (!countryObj) {
+        return <></>
+    } else {
+        //console.log(Object.values(countryObj.languages))
+        const langs = Object.values(countryObj.languages)
+        return (
+            <div>
+                <h2>
+                    {countryObj.name.common}
+                </h2>
+                
+                <p>capital {countryObj.capital[0]}</p>
+                <p>area {countryObj.area} </p>
+                <b>languages:</b>
+                <ul>
+                    {langs.map(lang =>
+                        <li key={lang}>
+                            {lang}
+                        </li>
+                    )}
+                </ul>
+            </div>
+        )
+    }
 }
 
 
