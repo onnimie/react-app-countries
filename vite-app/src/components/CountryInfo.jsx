2e7b295a5fc42f2ec52b6b538774bf2c0a5a6b94
onnimiettinen <onnimie@gmail.com>
2024-07-17 18:48:54 +0300

    create first version for CountryInfo

--- vite-app/src/components/CountryInfo.jsx
+++ vite-app/src/components/CountryInfo.jsx
@@ -6,6 +6,16 @@ const CountryInfo = ({countryObj}) => {
             </h2>
             
             <p>capital {countryObj.capital[0]}</p>
+            <p>area {countryObj.area} </p>
+            <br />
+            <b>languages:</b>
+            <ul>
+                {countryObj.languages.values().map(lang =>
+                    <li key={lang}>
+                        {lang}
+                    </li>
+                )}
+            </ul>
         </div>
     )
 }
