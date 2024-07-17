471c4cb45e262c7ecd489678ae5e01e710237d44
onnimiettinen <onnimie@gmail.com>
2024-07-17 19:58:13 +0300

    refactor App, create a state for weather info, and add the WeatherInfo component to the app

--- vite-app/src/App.jsx
+++ vite-app/src/App.jsx
@@ -3,6 +3,8 @@ import CountryInfo from './components/CountryInfo'
 import SearchBar from './components/SearchBar'
 import countriesAPI from './services/countriesAPI'
 import SearchOutput from './components/SearchOutput'
+import weatherAPI from './services/weatherAPI'
+import WeatherInfo from './components/WeatherInfo'
 
 
 
@@ -12,11 +14,36 @@ function App() {
   const [searchOutputList, setSearchOutputList] = useState([])
   const [searchOutputError, setSearchOutputError] = useState('')
   const [searchOutputCountry, setSearchOutputCountry] = useState(null)
+  const [searchOutputCountryWeather, setSearchOutputCountryWeather] = useState(null)
 
   const searchOutputThreshold = 10 //if output has more countries, render some message that says its too many
 
   const overThresholdMessage = "Too many matches, specify further"
   const noMatchMessage = "No country matches the specification"
+
+
+  const showSingleCountryInfo = name => {
+    countriesAPI.getCountryByName(name).then(res => {
+      setSearchOutputCountry(res)
+      setSearchOutputError('')
+      setSearchOutputList([])
+
+      const [lat,lon] = res.capitalInfo.latlng
+      weatherAPI.getWeatherWithLongitudeLatitude(lon,lat).then(res => {
+
+        setSearchOutputCountryWeather(res)
+
+      })
+      .catch(err => {
+        console.log("error in getWeatherWithLonLat", err)
+      })
+
+    })
+    .catch(err => {
+      console.log("error in getCountryByName", err)
+    })
+  }
+
 
   const handleSearchBarInputChange = (event) => {
     //console.log(event.target.value)
@@ -33,14 +60,7 @@ function App() {
           if (filteredNames.length === 1) {
             // show the country info for this country
 
-            countriesAPI.getCountryByName(filteredNames[0]).then(res => {
-              setSearchOutputCountry(res)
-              setSearchOutputError('')
-              setSearchOutputList([])
-            })
-            .catch(err => {
-              console.log("error in getCountryByName", err)
-            })
+            showSingleCountryInfo(filteredNames[0])
 
           } else {
 
@@ -49,6 +69,7 @@ function App() {
               setSearchOutputError(overThresholdMessage)
               setSearchOutputList([])
               setSearchOutputCountry(null)
+              setSearchOutputCountryWeather(null)
 
             } else {
 
@@ -56,6 +77,7 @@ function App() {
               setSearchOutputList(filteredNames)
               setSearchOutputError('')
               setSearchOutputCountry(null)
+              setSearchOutputCountryWeather(null)
             }
           }
         } else {
@@ -64,6 +86,7 @@ function App() {
           setSearchOutputList([])
           setSearchOutputError(noMatchMessage)
           setSearchOutputCountry(null)
+          setSearchOutputCountryWeather(null)
         }
 
 
@@ -74,14 +97,7 @@ function App() {
   }
 
   const showCountryButtonClicked = (name) => {
-    countriesAPI.getCountryByName(name).then(res => {
-      setSearchOutputCountry(res)
-      setSearchOutputError('')
-      setSearchOutputList([])
-    })
-    .catch(err => {
-      console.log("error in getCountryByName", err)
-    })
+    showSingleCountryInfo(name)
   }
 
   return (
@@ -97,6 +113,8 @@ function App() {
       <SearchOutput countryNames={searchOutputList} showFunction={showCountryButtonClicked} />
 
       <CountryInfo countryObj={searchOutputCountry} />
+
+      <WeatherInfo countryObj={searchOutputCountry} weatherObj={searchOutputCountryWeather} />
     </div>
   )
 }
