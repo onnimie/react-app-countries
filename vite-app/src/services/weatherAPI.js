cc61c66d30de02475610cbbdb3592108b8f7df1b
onnimiettinen <onnimie@gmail.com>
2024-07-17 19:57:03 +0300

    change incoming API data to follow metric units

---
+++ vite-app/src/services/weatherAPI.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+
+// Running the app with vite, add the API key to e.g. an environment variable by:
+// set "VITE_WEATHER_API_KEY=apikeyhere" && npm run dev
+
+// with this, we access it as follows:
+//const api_key = import.meta.env.VITE_WEATHER_API_KEY
+const api_key = '79801c3d3b0b1a06285f26742f69b9f4'
+
+
+const getWeatherWithLongitudeLatitude = (lon, lat) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`
+    const promiseResponse = axios.get(url).then(res => res.data)
+    return promiseResponse
+}
+
+
+export default {
+    getWeatherWithLongitudeLatitude
+}
