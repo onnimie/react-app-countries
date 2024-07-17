f8df2f8c22609416f66509449faa7d11ce126a98
onnimiettinen <onnimie@gmail.com>
2024-07-17 18:18:46 +0300

    add states for search output and error, and add conditions into handleSearchBarInputChange for rendering the search results (search everytime input changes)

--- vite-app/src/App.jsx
+++ vite-app/src/App.jsx
@@ -1,17 +1,61 @@
 import { useState } from 'react'
 //import CountryInfo from './components/CountryInfo'
 import SearchBar from './components/SearchBar'
+import countriesAPI from './services/countriesAPI'
 
 
 
 function App() {
 
   const [searchBarValue, setSearchBarValue] = useState('')
+  const [searchOutputList, setSearchOutputList] = useState([])
+  const [searchOutputError, setSearchOutputError] = useState('')
 
+  const searchOutputThreshold = 10 //if output has more countries, render some message that says its too many
+
+  const overThresholdMessage = "Too many matches, specify further"
+  const noMatchMessage = "No country matches the specification"
 
   const handleSearchBarInputChange = (event) => {
     //console.log(event.target.value)
     setSearchBarValue(event.target.value)
+
+    countriesAPI.getAllNamesCommon()
+      .then(res => {
+        if (res.length > 0) {
+
+          if (res.length === 1) {
+            // show the country info for this country
+
+            setSearchOutputError('Single MATCH!!')
+            setSearchOutputList([])
+
+          } else {
+
+            if (res.length > searchOutputThreshold) {
+              // too many countries to list
+              setSearchOutputError(overThresholdMessage)
+              setSearchOutputList([])
+
+            } else {
+
+              // output the countries
+              setSearchOutputList(res)
+              setSearchOutputError('')
+            }
+          }
+        } else {
+          // the list is empty, i.e., no matches
+
+          setSearchOutputList([])
+          setSearchOutputError(noMatchMessage)
+        }
+
+
+      })
+      .catch(err => {
+        console.log("Some error in getAllNamesCommon", err)
+      })
   }
 
   return (
